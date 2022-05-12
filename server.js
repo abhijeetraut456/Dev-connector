47aa60cedae3823e842b7b0069e35b94583ccc24
Abhijeet raut <Abhijeetraut456@gmail.com>
2022-05-12 18:23:58 +0530

    deploy

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
-const PORT = process.env.PORT || 5000;
-
 const path = require('path');
+
 //Connect Database
 connectDB();
 app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
@@ -21,7 +20,10 @@ app.use('/api/post', require('./routes/api/posts'));
 if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
+
+const PORT = process.env.PORT || 5000;
