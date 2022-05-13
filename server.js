cd8c2bb9831e6f293175b58d48965aacc7a1f0ba
Abhijeet raut <Abhijeetraut456@gmail.com>
2022-05-13 12:41:36 +0530

    deploying app

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 //Connect Database
 connectDB();
@@ -25,5 +26,3 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
-
-const PORT = process.env.PORT || 5000;
